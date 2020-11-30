2d827ee57984141f18a6a365b838275377ebfa3d
shubhamdamani <damani.shubham4@gmail.com>
2020-11-30 13:00:10 +0530

    input taken

--- app/src/main/java/com/example/facemaskdetection/LoginActivity.kt
+++ app/src/main/java/com/example/facemaskdetection/LoginActivity.kt
@@ -4,14 +4,35 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.system.Os.uname
 import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import com.example.aps1.R
 import com.example.facemaskdetection.model.Police
+import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         Police.uname = "Raj";
-        Log.e("TAG",Police.uname);
+        Log.e("TAG",Police.uname)
+        //val db = Firebase.firestore
+        val textView: TextView = findViewById(R.id.loginID) as TextView
+
+        val textView2: TextView = findViewById(R.id.loginPSW) as TextView
+
+
+        val button: Button =findViewById(R.id.loginBTN) as Button
+        button.setOnClickListener(View.OnClickListener {
+            val str: String = textView.text.toString().trim()
+            val str2: String = textView2.text.toString().trim()
+
+
+
+        })
     }
 }
