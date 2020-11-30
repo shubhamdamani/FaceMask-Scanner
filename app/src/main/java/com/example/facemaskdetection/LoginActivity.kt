e4f99be38007500379bc758e5d3653111a450a21
rrajgarg <rajgarg150873@gmail.com>
2020-11-30 14:14:46 +0530

    check added

--- app/src/main/java/com/example/facemaskdetection/LoginActivity.kt
+++ app/src/main/java/com/example/facemaskdetection/LoginActivity.kt
@@ -6,6 +6,7 @@ import android.system.Os.uname
 import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.CheckBox
 import android.widget.TextView
 import com.example.aps1.R
 import com.example.facemaskdetection.model.Police
@@ -25,29 +26,35 @@ class LoginActivity : AppCompatActivity() {
         val textView: TextView = findViewById(R.id.loginID) as TextView
 
         val textView2: TextView = findViewById(R.id.loginPSW) as TextView
-
+        var chk: CheckBox
+        chk= findViewById(R.id.loginCB)
         val TAG = "RajGarg";
         val button: Button =findViewById(R.id.loginBTN) as Button
         button.setOnClickListener(View.OnClickListener {
             val str: String = textView.text.toString().trim()
             val str2: String = textView2.text.toString().trim()
-            db.collection("police")
-                .get()
-                .addOnSuccessListener { result ->
-                    for (document in result) {
-                        Log.d(TAG, "${document.id} => ${document.data}")
-                        if(document.data["uname"] == str && document.data["upwd"] == str2){
-                            Police.uname = document.data["uname"].toString()
-                            Police.signal = document.data["signal"].toString()
-                            Police.uid = document.data["uid"].toString()
-                            Police.upwd = document.data["upwd"].toString()
-                            Log.d(TAG,"Successful!!!")
+            if(chk.isChecked){
+
+            }
+            else{
+                db.collection("police")
+                    .get()
+                    .addOnSuccessListener { result ->
+                        for (document in result) {
+                            Log.d(TAG, "${document.id} => ${document.data}")
+                            if(document.data["uname"] == str && document.data["upwd"] == str2){
+                                Police.uname = document.data["uname"].toString()
+                                Police.signal = document.data["signal"].toString()
+                                Police.uid = document.data["uid"].toString()
+                                Police.upwd = document.data["upwd"].toString()
+                                Log.d(TAG,"Successful!!!")
+                            }
                         }
                     }
-                }
-                .addOnFailureListener { exception ->
-                    Log.w(TAG, "Error getting documents.", exception)
-                }
+                    .addOnFailureListener { exception ->
+                        Log.w(TAG, "Error getting documents.", exception)
+                    }
+            }
         })
     }
 }
