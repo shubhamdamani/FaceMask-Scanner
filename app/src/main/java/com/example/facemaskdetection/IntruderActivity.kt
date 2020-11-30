5b7468af87ad75d341eaac337c7a0cb52c8e91bb
shubhamdamani <damani.shubham4@gmail.com>
2020-11-30 14:12:38 +0530

    all activities created

---
+++ app/src/main/java/com/example/facemaskdetection/IntruderActivity.kt
@@ -0,0 +1,12 @@
+package com.example.facemaskdetection
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.aps1.R
+
+class IntruderActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intruder)
+    }
+}
---
+++ app/src/main/java/com/example/facemaskdetection/model/Signal.kt
@@ -0,0 +1,10 @@
+package com.example.facemaskdetection.model
+
+class Signal{
+    companion object{
+        var uname="uname"
+        var upwd="upwd"
+        var uid="uid"
+        var signal= listOf<String>()
+    }
+}
