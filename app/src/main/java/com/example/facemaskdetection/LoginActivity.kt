817db7689756243471707bdcf2d0ccdf398c56f0
rrajgarg <rajgarg150873@gmail.com>
2020-11-30 14:02:07 +0530

    Retrieving from firestore

--- app/src/main/java/com/example/facemaskdetection/LoginActivity.kt
+++ app/src/main/java/com/example/facemaskdetection/LoginActivity.kt
@@ -9,6 +9,7 @@ import android.widget.Button
 import android.widget.TextView
 import com.example.aps1.R
 import com.example.facemaskdetection.model.Police
+import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
@@ -20,19 +21,33 @@ class LoginActivity : AppCompatActivity() {
         setContentView(R.layout.activity_login)
         Police.uname = "Raj";
         Log.e("TAG",Police.uname)
-        //val db = Firebase.firestore
+        val db = Firebase.firestore
         val textView: TextView = findViewById(R.id.loginID) as TextView
 
         val textView2: TextView = findViewById(R.id.loginPSW) as TextView
 
-
+        val TAG = "RajGarg";
         val button: Button =findViewById(R.id.loginBTN) as Button
         button.setOnClickListener(View.OnClickListener {
             val str: String = textView.text.toString().trim()
             val str2: String = textView2.text.toString().trim()
-
-
-
+            db.collection("police")
+                .get()
+                .addOnSuccessListener { result ->
+                    for (document in result) {
+                        Log.d(TAG, "${document.id} => ${document.data}")
+                        if(document.data["uname"] == str && document.data["upwd"] == str2){
+                            Police.uname = document.data["uname"].toString()
+                            Police.signal = document.data["signal"].toString()
+                            Police.uid = document.data["uid"].toString()
+                            Police.upwd = document.data["upwd"].toString()
+                            Log.d(TAG,"Successful!!!")
+                        }
+                    }
+                }
+                .addOnFailureListener { exception ->
+                    Log.w(TAG, "Error getting documents.", exception)
+                }
         })
     }
 }
